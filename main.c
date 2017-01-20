0465e43308b019eab77cb9d0294e1acf5d906f28
solin.guo <solin.guo@tensormicro.com>
2017-01-20 16:18:02 +0800

    Delete the invalid judgment

--- main.c
+++ main.c
@@ -676,33 +676,21 @@ int uECC_make_key(uint8_t *public_key,
     uECC_word_t _public[uECC_MAX_WORDS * 2];
     uECC_word_t tries;
 
-    for (tries = 0; tries < uECC_RNG_MAX_TRIES; ++tries) {
-//        if (!uECC_generate_random_int(_private, curve->n, BITS_TO_WORDS(curve->num_n_bits))) {
-//            return 0;
-//        }
-        _private[5] = 0x07915f86;
-        _private[4] = 0x918ddc27;
-        _private[3] = 0x005df1d6;
-        _private[2] = 0xcf0c142b;
-        _private[1] = 0x625ed2ef;
-        _private[0] = 0xf4a518ff;
+    if (EccPoint_compute_public_key(_public, _private, curve)) {
+        uECC_vli_nativeToBytes(private_key, BITS_TO_BYTES(curve->num_n_bits), _private);
+        uECC_vli_nativeToBytes(public_key, curve->num_bytes, _public);
+        printf("_private:\n");
+        for (_i = 0 ;_i < 6 ;_i++)
+            printf("%x" ,_private[5-_i]);
 
-        if (EccPoint_compute_public_key(_public, _private, curve)) {
-            uECC_vli_nativeToBytes(private_key, BITS_TO_BYTES(curve->num_n_bits), _private);
-            uECC_vli_nativeToBytes(public_key, curve->num_bytes, _public);
-            printf("_private:\n");
-            for (_i = 0 ;_i < 6 ;_i++)
-                printf("%x" ,_private[5-_i]);
-
-            printf("\nprivate_key:\n");
-            for (_i = 0 ;_i < 24 ;_i++) {
-                printf("%x" ,private_key[_i]);
-            }
-            printf("\n");
-            uECC_vli_nativeToBytes(
-                public_key + curve->num_bytes, curve->num_bytes, _public + curve->num_words);
-            return 1;
+        printf("\nprivate_key:\n");
+        for (_i = 0 ;_i < 24 ;_i++) {
+            printf("%x" ,private_key[_i]);
         }
+        printf("\n");
+        uECC_vli_nativeToBytes(
+            public_key + curve->num_bytes, curve->num_bytes, _public + curve->num_words);
+        return 1;
     }
     return 0;
 }
